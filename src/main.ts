615fc9aa07c69731f4ec8ccd94068414856a5fcc
Kareem Atout <114629466+kareematout@users.noreply.github.com>
2025-01-08 20:10:59 -0800

    Added files

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+
+bootstrapApplication(AppComponent, {
+   providers: [provideHttpClient(
+      // withInterceptors([])
+   )]
+}).catch((err) => console.error(err));
